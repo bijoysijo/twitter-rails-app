14a1640fbd0c013fedc371d15d1398f40a08821f
Bijoy Sijo <bijoysijo21@gmail.com>
2020-06-07 20:30:11 +0530

    edit and update actions

--- app/controllers/tweeets_controller.rb
+++ app/controllers/tweeets_controller.rb
@@ -1,2 +1,44 @@
 class TweeetsController < ApplicationController
+
+  def index
+    @tweeets = Tweeet.all
+  end
+
+  def new
+    @tweeet = Tweeet.new
+  end
+
+  def create
+    @tweeet = Tweeet.new(tweeet_params)
+
+    if @tweeet.save
+      redirect_to @tweeet
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @tweeet = Tweeet.find(params[:id])
+  end
+
+  def edit
+    @tweeet = Tweeet.find(params[:id])
+  end
+
+  def update
+    @tweeet = Tweeet.find(params[:id])
+
+    if @tweeet.update(tweeet_params)
+      redirect_to @tweeet
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def tweeet_params
+    params.require(:tweeet).permit(:tweeet)
+  end
 end
